399671ba1a811f5ac25f41d3b64faea5f1990b1b
Priansh Shah <shah@priansh.com>
2017-11-15 04:37:49 +0000

    improved arrow funcs

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-var buddypress = require( "buddypress" );
+var buddypress = require("buddypress");
 var client = buddypress.createClient({
-	url: "fawkesbots.com",
-	username: "guyfawkes",
-	password: "houstonwehaveaproblem$"
+	url: "vivliolearning.com",
+	username: "admin",
+	password: "1892Dragon"
 });
 
-client.verifyConnection(function( error, posts ) {
-	console.log( "Found " + posts + " posts!" );
-});
+/* client.verifyConnection(function (e, result) {
+	if (e) console.log(e)
+	else if (result.confirmation) console.log("Success")
+	free=true
+})
+ */
+client.getProfile( function (e,r) {
+	if(e) console.log(e)
+	else console.log(r)
+})
